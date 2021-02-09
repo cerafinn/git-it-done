55ef746adaf66af043dca80a791065a1f48212d9
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-02-09 18:04:52 -0500

    fetch list of repos by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,5 @@
 var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
@@ -22,6 +23,14 @@ var getUserRepos = function(user) {
     alert("Unable to connect to GitHub");
   })
 };
+
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute("data-language");
+  if(language) {
+    getFeaturedRepos(language)
+    repoContainerEl.textContent= "";
+  }
+}
 
 var formSubmitHandler = function(event) {
   event.preventDefault();
@@ -71,4 +80,20 @@ var displayRepos = function(repos, searchTerm) {
   }
 }
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl)
+  .then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language)
+      });
+    } else {
+      alert("Error: " + response.statusText)
+    }
+  });
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
